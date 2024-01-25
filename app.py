bcbd3b0d4e4a6e17a245b0da2a2a7fb015718575
lob0scuro <kamrin717@gmail.com>
2024-01-25 09:24:37 -0600

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
 from config import *
 
-
+test=123
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/udt"
